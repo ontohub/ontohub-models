16d15363ed413fdd9f9508bd143fa797b05cf428
Tom Gehrke <phyrog@cognitive-coding.com>
2016-09-24 22:33:30 +0200

    Remove code that is not yet needed

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,4 @@ class User < OrganizationalUnit
       graph(:namespaces, {id: :namespace_id}, select: false).
       where(namespaces__organizational_unit_id: id)
   end
-
-  def groups_repositories
-    Repository.
-      graph(:namespaces, {id: :namespace_id}, select: false).
-      graph(:group_memberships,
-            {group_id: :namespaces__organizational_unit_id},
-            select: false).
-      where(group_memberships__user_id: id)
-  end
 end
