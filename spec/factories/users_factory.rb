3658e180f001757603fb18c899944084fd98eb21
Tom Gehrke <phyrog@cognitive-coding.com>
2016-09-24 22:28:16 +0200

    Add user specs

--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -1,8 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    display_name "MyString"
-    email "MyString"
-    password "MyString"
-    secret "MyString"
+    name { Faker::Internet.user_name }
+    created_at { Time.current }
+    updated_at { Time.current }
+    display_name { Faker::Name.last_name }
+    email { Faker::Internet.email(name) }
+    password { Faker::Internet.password }
+    secret { Faker::Crypto.sha1 }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
+require 'devise'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'columns' do
+    it { is_expected.to have_column(:display_name, type: :string) }
+    it { is_expected.to have_column(:email, type: :string) }
+    it { is_expected.to have_column(:encrypted_password, type: :string) }
+  end
+
+  context 'password' do
+    subject { create :user, password: 'foobar' }
+
+    it 'saved the password encrypted' do
+      expect(subject.encrypted_password.length).to be(60)
+      expect(subject.encrypted_password).to match(/^\$2a\$/)
+    end
+
+    it 'validates the password correctly' do
+      expect(subject.valid_password?('foobar')).to be true
+      expect(subject.valid_password?('barfoo')).to be false
+    end
+
+    it 'updates the encrypted password when the password is updated' do
+      old_pw = subject.encrypted_password
+      subject.password = 'barfoo'
+      expect(subject.encrypted_password).not_to equal(old_pw)
+
+    end
+  end
+
+  context 'personal repositories' do
+    subject { create :user }
+
+    before do
+      2.times { create :repository, namespace: subject.namespace }
+      2.times { create :repository, namespace: create(:user).namespace }
+    end
+
+    it 'lists all personal repositories' do
+      expect(subject.personal_repositories.count).to be(2)
+    end
+  end
 end
