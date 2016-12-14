ee4ea1290a7ebc48e7f3f4cad2b98f5605c5a094
Eugen Kuksa <kuksa.eugen@gmail.com>
2016-12-14 10:09:51 +0100

    Use an association with a subquery to select accessible repositories.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,9 @@ class User < OrganizationalUnit
   many_to_many :organizations,
     join_table: :organizations_members, left_key: :member_id
 
-  def accessible_repositories
-    repositories + organizations.map(&:repositories).flatten
-  end
+  one_to_many :accessible_repositories, dataset: (proc do |reflection|
+    reflection.associated_dataset.
+      where(owner_id: id).
+      or(owner_id: organizations_dataset.select(:organizational_units__id))
+  end), class: Repository
 end
