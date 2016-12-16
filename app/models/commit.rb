5fbfbca3233a2502394a3f07734d1fe8308e003a
Eileen Bolloff <ebolloff@informatik.uni-bremen.de>
2016-12-16 14:59:38 +0100

    Add automatic editor and author setters via email and validations.

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -2,10 +2,29 @@
 
 # A specific commit belongs to a repository and can have many file versions.
 class Commit < Sequel::Model
+  include ModelWithURL
+
   plugin :timestamps
+  plugin :validation_helpers
 
   many_to_one :repository
-  one_to_many :file_version
-  many_to_one :author, key: :author_id, class: :User
-  many_to_one :editor, key: :editor_id, class: :User
+  one_to_many :file_versions
+  many_to_one :author, class: :User
+  many_to_one :editor, class: :User
+  many_to_one :pusher, class: :User
+
+  def author_email=(author_email)
+    super(author_email)
+    self.author = User.find(email: author_email)
+  end
+
+  def editor_email=(editor_email)
+    super(editor_email)
+    self.editor = User.find(email: editor_email)
+  end
+
+  def validate
+    validates_presence :pusher
+    super
+  end
 end
--- app/models/file_version.rb
+++ app/models/file_version.rb
@@ -2,7 +2,15 @@
 
 # Represents a specific version of a file.
 class FileVersion < Sequel::Model
+  include ModelWithURL
+
   plugin :timestamps
+  plugin :validation_helpers
 
   many_to_one :commit
+
+  def validate
+    validates_presence :commit
+    super
+  end
 end
