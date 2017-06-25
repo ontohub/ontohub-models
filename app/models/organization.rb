434b8a3fcc0570de8abde6ad3232f20535d15567
Tom Gehrke <phyrog@cognitive-coding.com>
2017-06-25 10:57:59 +0200

    Add association dependencies

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,4 +4,6 @@
 class Organization < OrganizationalUnit
   many_to_many :members, class: User, join_table: :organization_memberships
   one_to_many :organization_memberships
+
+  plugin :association_dependencies, members: :nullify
 end
