de487579bb5c62b326d348babd1b1b2d02a7c207
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-08-11 17:29:32 +0200

    Be specific about NULL, add defaults, re-order the definitions

--- db/migrate/20160902111740_initial_schema.rb
+++ db/migrate/20160902111740_initial_schema.rb
@@ -3,137 +3,151 @@
 Sequel.migration do
   change do
     create_enum :organizational_unit_kind_type,
-      %w(OrganizationalUnit User Organization)
+      %w(User Organization)
+
     create_table :organizational_units do
       primary_key :id
       # Kind of record - for class table inheritance
-      column :kind, :organizational_unit_kind_type
+      column :kind, :organizational_unit_kind_type, null: false
 
-      column :url_path, String, unique: true
+      column :slug, String, null: false, unique: true
+      column :url_path, String, null: false, unique: true
 
-      column :slug, String, unique: true
       column :display_name, String, null: true
 
-      column :created_at, DateTime
-      column :updated_at, DateTime
+      column :created_at, DateTime, null: false,
+                                    default: Sequel::CURRENT_TIMESTAMP
+      column :updated_at, DateTime, null: true
     end
 
     # User is an OrganizationalUnit
     create_enum :global_role,
       %w(admin user)
+
     create_table :users do
       primary_key :id
-      foreign_key [:id], :organizational_units, unique: true
+      foreign_key [:id], :organizational_units, null: false, unique: true
 
-      column :secret, String
+      column :secret, String, null: true
 
       # Devise database authenticatable
-      column :email, String, unique: true
-      column :encrypted_password, String
+      column :email, String, null: false, unique: true
+      column :encrypted_password, String, null: false
 
       # Devise confirmable
-      column :confirmation_token, String
-      column :confirmed_at, DateTime
-      column :confirmation_sent_at, DateTime
-      column :unconfirmed_email, String
+      column :confirmation_token, String, null: true
+      column :confirmed_at, DateTime, null: true
+      column :confirmation_sent_at, DateTime, null: true
+      column :unconfirmed_email, String, null: true
 
       # Devise recoverable
-      column :reset_password_token, String
-      column :reset_password_sent_at, DateTime
+      column :reset_password_token, String, null: true
+      column :reset_password_sent_at, DateTime, null: true
 
       # Devise lockable
-      column :failed_attempts, Integer, default: 0
-      column :unlock_token, String
-      column :locked_at, DateTime
+      column :failed_attempts, Integer, null: true, default: 0
+      column :unlock_token, String, null: true
+      column :locked_at, DateTime, null: true
 
       # Devise trackable
-      column :sign_in_count, Integer, default: 0
-      column :current_sign_in_at, DateTime
-      column :last_sign_in_at, DateTime
+      column :sign_in_count, Integer, null: true, default: 0
+      column :current_sign_in_at, DateTime, null: true
+      column :last_sign_in_at, DateTime, null: true
       # Devise requires these columns to be set, but we don't want to store the
       # IP addresses in the database. We define empty getters/setters in the
       # model for these attributes:
       # column :current_sign_in_ip, String
       # column :last_sign_in_ip, String
 
-      column :role, :global_role, default: 'user'
+      column :role, :global_role, null: false, default: 'user'
+    end
+
+    create_table :public_keys do
+      primary_key :id
+      foreign_key :user_id, :users, null: false, index: true
+
+      column :name, String, null: false
+      column :key, String, null: false
+
+      column :created_at, DateTime, null: false,
+                                    default: Sequel::CURRENT_TIMESTAMP
+      column :updated_at, DateTime, null: true
     end
 
     # Organization is a OrganizationalUnit
     create_table :organizations do
       primary_key :id
-      foreign_key [:id], :organizational_units, unique: true
-      column :description, String
+      foreign_key [:id], :organizational_units, null: false, unique: true
+      column :description, String, null: false
     end
 
     create_enum :organization_role,
       %w(admin write read)
+
     create_table :organization_memberships do
       primary_key [:organization_id, :member_id]
-      foreign_key :organization_id, :organizations, index: true
-      foreign_key :member_id, :users, index: true
-      column :role, :organization_role, default: 'read'
+      foreign_key :organization_id, :organizations, null: false, index: true
+      foreign_key :member_id, :users, null: false, index: true
+      column :role, :organization_role, null: false, default: 'read'
     end
 
     create_enum :repository_content_type,
       %w(ontology model specification mathematical)
+
     create_table :repositories do
       primary_key :id
-      foreign_key :owner_id, :organizational_units,
-                  index: true, null: false
+      column :slug, String, null: false, unique: true
+      foreign_key :owner_id, :organizational_units, null: false, index: true
 
       column :url_path, String, unique: true
 
-      column :name, String
-      column :slug, String, unique: true
-      column :description, :text
-      column :created_at, DateTime
-      column :updated_at, DateTime
-      column :public_access, TrueClass
-      column :content_type, :repository_content_type
+      column :name, String, null: false
+      column :description, :text, null: true
+      column :public_access, TrueClass, null: false
+      column :content_type, :repository_content_type, null: false
+
+      column :created_at, DateTime, null: false,
+                                    default: Sequel::CURRENT_TIMESTAMP
+      column :updated_at, DateTime, null: true
     end
 
     create_enum :repository_role,
       %w(admin write read)
+
     create_table :repository_memberships do
       primary_key [:repository_id, :member_id]
-      foreign_key :repository_id, :repositories, index: true
-      foreign_key :member_id, :users, index: true
-      column :role, :repository_role, default: 'read'
+      foreign_key :repository_id, :repositories, null: false, index: true
+      foreign_key :member_id, :users, null: false, index: true
+      column :role, :repository_role, null: false, default: 'read'
     end
 
     create_enum :loc_id_base_kind_type,
       %w(FileVersion)
+
     create_table :loc_id_bases do
       primary_key :id
       # Kind of record - for class table inheritance
-      column :kind, :loc_id_base_kind_type
+      column :kind, :loc_id_base_kind_type, null: false
 
       # The actual Loc/Id is saved in url_path to stay consistent throughout the
       # code.
-      column :url_path, String, unique: true
-      column :created_at, DateTime
-      column :updated_at, DateTime
+      column :url_path, String, null: false, unique: true
+      column :created_at, DateTime, null: false,
+                                    default: Sequel::CURRENT_TIMESTAMP
+      column :updated_at, DateTime, null: true
     end
 
     # FileVersion is a LocIdBase
     create_table :file_versions do
       primary_key :id
-      foreign_key [:id], :loc_id_bases, unique: true
+      foreign_key [:id], :loc_id_bases, null: false, unique: true
 
-      foreign_key :repository_id, :repositories, index: true, null: false
+      foreign_key :repository_id, :repositories, null: false, index: true
 
       column :commit_sha, String, null: false
       column :path, String, null: false
-      index [:repository_id, :commit_sha, :path], unique: true
-    end
 
-    create_table :public_keys do
-      primary_key :id
-      foreign_key :user_id, :users, index: true, null: false
-
-      column :name, String, null: false
-      column :key, String, null: false
+      index [:repository_id, :commit_sha, :path], null: false, unique: true
     end
   end
 end
--- spec/factories/public_keys_factory.rb
+++ spec/factories/public_keys_factory.rb
@@ -5,5 +5,7 @@ FactoryGirl.define do
     association :user, factory: :user
     name { Faker::Cat.name }
     key { Faker::Crypto.sha256 }
+    created_at { Time.current }
+    updated_at { Time.current }
   end
 end
