5c43e364fd84c2022361f72e44cceb4a126e0eb7
Tom Gehrke <phyrog@users.noreply.github.com>
2017-09-01 14:37:19 +0200

    Add public key validations and tests (#100)
    * Add public key validations and tests
    
    * Obey review

--- app/models/public_key.rb
+++ app/models/public_key.rb
@@ -3,4 +3,30 @@
 # Class to hold a user's public key
 class PublicKey < Sequel::Model
   many_to_one :user
+  plugin :validation_helpers
+
+  def before_validation
+    self.fingerprint = key_fingerprint
+  end
+
+  def validate
+    validates_presence [:key, :name, :user]
+
+    if new? && PublicKey.find(user_id: user_id, name: name)
+      errors.add(:name, 'is already taken')
+    end
+
+    errors.add(:key, 'is invalid') unless fingerprint
+  end
+
+  private
+
+  def key_fingerprint
+    return @key_fingerprint if @key_fingerprint
+    return nil if key.blank?
+    decoded_key = Base64.strict_decode64(key.split(' ').last)
+    @key_fingerprint = Digest::MD5.hexdigest(decoded_key)
+  rescue ArgumentError => _
+    nil
+  end
 end
--- spec/models/public_key_spec.rb
+++ spec/models/public_key_spec.rb
@@ -7,4 +7,58 @@ RSpec.describe PublicKey, type: :model do
     subject { build(:public_key) }
     it_behaves_like 'an object with timestamps'
   end
+
+  context 'validations' do
+    subject { build(:public_key) }
+
+    it 'is valid' do
+      expect(subject).to be_valid
+    end
+
+    context 'name' do
+      it 'is invalid if the name is already taken' do
+        create(:public_key, name: subject.name, user: subject.user)
+        expect(subject).not_to be_valid
+      end
+
+      it 'is invalid if the name is not present' do
+        subject.name = ''
+        expect(subject).not_to be_valid
+      end
+    end
+
+    context 'key' do
+      it 'is invalid if the key is not present' do
+        subject.key = ''
+        expect(subject).not_to be_valid
+      end
+    end
+
+    context 'user' do
+      it 'is invalid if the user is not present' do
+        subject.user = nil
+        expect(subject).not_to be_valid
+      end
+    end
+  end
+
+  context 'fingerprint' do
+    context 'valid key' do
+      subject { create(:public_key) }
+
+      it 'has a fingerprint after creating' do
+        expect(subject.fingerprint).not_to be_empty
+      end
+    end
+
+    context 'invalid key' do
+      subject { build(:public_key, key: 'ssh-rsa someinvalidkey') }
+
+      before { subject.before_validation }
+
+      it 'is nil' do
+        expect(subject.fingerprint).to be_nil
+      end
+    end
+  end
 end
