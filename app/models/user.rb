530ee5c30cc1ae286aa09c2ae2e8ba59bc498607
Tom Gehrke <phyrog@cognitive-coding.com>
2017-10-05 09:45:20 +0200

    Remove wrongful caching

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < OrganizationalUnit
   # equivalent to
   # organization_memberships.where(role: role).map(&:organization)
   def organizations_by_role(role)
-    @organizations_by_role_dataset ||= Organization.dataset.
+    Organization.dataset.
       join(:organization_memberships, {organization_id: :id}, select: false).
       where(Sequel[:organization_memberships][:role] => role,
             Sequel[:organization_memberships][:member_id] => id)
