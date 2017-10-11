7247984dba7e3601045c10813461e4b384372081
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-10-11 17:39:36 +0200

    Add Document import associations.

--- app/models/document.rb
+++ app/models/document.rb
@@ -17,4 +17,18 @@ class Document < LocIdBase
       where(Sequel[:document_links][:target_id] => id).
       or(Sequel[:document_links][:source_id] => id)
   end), class: DocumentLink
+
+  # Documents that import this one
+  many_to_many :imported_by,
+    left_key: :target_id,
+    right_key: :source_id,
+    join_table: :document_links,
+    class: Document
+
+  # Documents that this one imports
+  many_to_many :imports,
+    left_key: :source_id,
+    right_key: :target_id,
+    join_table: :document_links,
+    class: Document
 end
