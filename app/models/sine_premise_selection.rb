19513ea69af4ecb99d0545e4b649c86269f52bf7
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-11-01 16:49:44 +0100

    Add SInE associations.

--- app/models/sine_premise_selection.rb
+++ app/models/sine_premise_selection.rb
@@ -2,4 +2,6 @@
 
 # The SinePremiseSelection is an automatic parameterised premise selection
 class SinePremiseSelection < PremiseSelection
+  one_to_many :sine_symbol_premise_triggers
+  one_to_many :sine_symbol_commonnesses
 end
--- spec/models/sine_premise_selection_spec.rb
+++ spec/models/sine_premise_selection_spec.rb
@@ -7,4 +7,40 @@ RSpec.describe SinePremiseSelection do
     subject { build(:sine_premise_selection) }
     it_behaves_like 'a premise_selection'
   end
+
+  context 'associations' do
+    subject { create(:sine_premise_selection) }
+
+    context 'sine_symbol_commonness' do
+      let!(:unrelated) do
+        (1..2).map { create(:sine_symbol_commonness) }
+      end
+
+      let!(:related) do
+        (1..2).map do
+          create(:sine_symbol_commonness, sine_premise_selection: subject)
+        end
+      end
+
+      it 'has the sentences' do
+        expect(subject.sine_symbol_commonnesses).to match_array(related)
+      end
+    end
+
+    context 'sine_symbol_premise_triggers' do
+      let!(:unrelated) do
+        (1..2).map { create(:sine_symbol_premise_trigger) }
+      end
+
+      let!(:related) do
+        (1..2).map do
+          create(:sine_symbol_premise_trigger, sine_premise_selection: subject)
+        end
+      end
+
+      it 'has the sentences' do
+        expect(subject.sine_symbol_premise_triggers).to match_array(related)
+      end
+    end
+  end
 end
