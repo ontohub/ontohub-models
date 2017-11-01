63312bef298241ecd6c2c0f408ec179d0194af78
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-11-01 16:49:34 +0100

    Remove bogus association.

--- app/models/reasoning_attempt.rb
+++ app/models/reasoning_attempt.rb
@@ -6,7 +6,6 @@ class ReasoningAttempt < Sequel::Model
 
   many_to_one :reasoner_configuration
   many_to_one :used_reasoner, class: Reasoner
-  one_to_many :reasoning_attempts
   one_to_many :generated_axioms
   one_to_one :reasoner_output
 end
