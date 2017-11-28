d0e73bf1f6d55e9007c5da5d5bbfff75cffffb27
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-11-28 11:49:27 +0100

    Fix mapping type enum. (#137)

--- db/migrate/20160902111740_initial_schema.rb
+++ db/migrate/20160902111740_initial_schema.rb
@@ -450,10 +450,8 @@ Sequel.migration do
          hiding_def
          free_def cofree_def np_free_def minimize_def
          hiding_open hiding_proved
-         hiding_free_open hiding_cofree_open
-         hiding_np_free_open hiding_minimize_open
-         hiding_free_proved hiding_cofree_proved
-         hiding_np_free_proved hiding_minimize_proved)
+         free_open cofree_open np_free_open minimize_open
+         free_proved cofree_proved np_free_proved minimize_proved)
 
     create_table :mappings do
       primary_key :id, type: :bigserial
