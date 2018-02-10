3bf6fdd3add021151275bbfd51f05559866db5b3
Eugen Kuksa <kuksa.eugen@gmail.com>
2018-02-10 22:33:54 +0100

    Allow for more public keys
    Currently, there are one 28 cat names available. We need more public
    keys in the tests, though.

--- spec/factories/public_keys_factory.rb
+++ spec/factories/public_keys_factory.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :public_key do
     association :user, factory: :user
-    name { Faker::Cat.unique.name }
+    name { Faker::Lorem.unique.word }
     key { 'ssh-rsa ' + Base64.strict_encode64(Faker::Crypto.sha256) }
   end
 end
